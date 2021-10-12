3458bbd243f7d8002d6727760ddb5675c3c8de52
Jairo Duarte <jd-nextstep@users.noreply.github.com>
2021-10-11 21:51:31 -0300

    arrays, simple

--- src/arrays.js
+++ src/arrays.js
@@ -3,12 +3,12 @@ export function createList() {
   return (
     "<ul>" +
     alunos
-      .map((umAlunoOriginal) => umAlunoOriginal.toUpperCase())
-      .filter((umAluno) => !umAluno.startsWith("Z"))
-      .map((umAlunoFiltrado) => `<li>${umAlunoFiltrado}</li>`)
+      .map((umAlunoOriginal) => umAlunoOriginal.toUpperCase()) //["LUIZINHO", "UGUINHO", "ZEZINHO", "FULANINHO"]
+      .filter((umAluno) => !umAluno.startsWith("Z")) //["LUIZINHO", "UGUINHO","FULANINHO"]
+      .map((umAlunoFiltrado) => `<li>${umAlunoFiltrado}</li>`) //["<li>LUIZINHO</li>", "<li>UGUINHO</li>","<li>FULANINHO</li>"]
       .reduce((acc, umAlunoLi) => {
         return acc.concat(umAlunoLi);
-      }) +
+      }) + // <li>LUIZINHO</li><li>UGUINHO</li><li>FULANINHO</li>
     "</ul>"
   );
 }
