6fdc804f6d2a843a9f12e3d9a8c79c641be2185c
Jairo Duarte <jd-nextstep@users.noreply.github.com>
2021-10-12 21:47:02 -0300

    functions HOF

--- src/functions.js
+++ src/functions.js
@@ -24,11 +24,25 @@ const singleParamFunc = (firstName) => {
   return firstName;
 };
 // super simple
-const superSmall = () => /* no log */ "hello there.";
+const superSmall = () => /* no log */ "hello there..";
+
+//functions are first class citzen, dont do this crap, adding a property to a function object
+superSmall.extraprop = "hidden";
+console.log(`extrapop=${superSmall.extraprop}`);
 
 //HOF
 const add = (x) => (y) => x + y;
-console.log(add(1)(3));
+console.log("add=" + add(1)(3));
+
+//HOF, but more clear
+function add2(x) {
+  //x = x * 2;
+  return function (y) {
+    return x + y;
+  };
+}
+const nameLessFunction = add2(1);
+console.log("nameLessFunction=" + nameLessFunction(3));
 
 //CallBack
 const conc = (a, b) => a + "" + b;
