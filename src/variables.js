978bae44d8bf21612458df701bd311a34e00f1fc
Jairo Duarte <24768314+jairo-duarte@users.noreply.github.com>
2021-11-01 14:16:51 -0300

    traduzir, e mais comentários

--- src/variables.js
+++ src/variables.js
@@ -1,9 +1,10 @@
 console.group("variables");
-// test declarado com uma função tradicional, o que permite a invocar antes da sua 'declaração'
-nameGlobal = "init"; // é possível usar var antes de declarar (Hoisting)
-//nuncaDeclarado = "outro init"; mas se nunca for declarado, a execução será suspensa por erro
+
+// test declarado com uma função tradicional, o que permite a invocar antes da sua 'declaração
 test("primeiro"); // nesta execução, o name e nameGlobal serão undefined
+nameGlobal = "init"; // é possível usar var antes de declarar (Hoisting)
 console.debug("valor de nameGlobal no escopo raiz=" + nameGlobal);
+//nuncaDeclarado = "outro init"; mas se nunca for declarado, a execução será suspensa por erro
 
 const name = "John Doe";
 var nameGlobal = "John Doe Global";
@@ -24,14 +25,17 @@ function test(where = "default where") {
   console.log(
     "where=" + where,
     "name=" +
-      (typeof name === "undefined"
+      (typeof name ===
+      "undefined" /* for segura de testar existencia de uma var*/
         ? "NA"
-        : name) /* name is a const, wont be visible here, but nameGlobal is declared with var, thus will be visible */,
+        : name) /* name é uma const, será acesspivel depois declarado, mas nameGlobal é um var */,
     "nameGlobal=" + nameGlobal,
     "age=" + age,
     "occup=" + occupation,
     "ageOfMarriage=" + ageOfMarriage
   );
+  // esta função usa informações declaradas fora de seu contexto/espoco, evitem isso.
+  // demonstrado aqui apenas para testes de escopos.
 }
 
 // here, ageOfFirstBorn is undefined
@@ -42,7 +46,7 @@ if (true) {
 }
 console.log("age=" + age);
 
-console.log("ageOfFirstBorn=" + ageOfFirstBorn); //works, var is global
-//console.log('ageOfMarriage=' + ageOfMarriage );  //doesnt, let is local to it´s scope
+console.log("ageOfFirstBorn=" + ageOfFirstBorn); //funciona, var é global
+//console.log("ageOfMarriage=" + ageOfMarriage); //não func, pois foi declarado como let, que fica preso ao seu escopo
 
 console.groupEnd();
